b446fc4a34bc97352c2dd8d20b0fc4800282d126
Alfaepsilon <36788922+Alfaepsilon@users.noreply.github.com>
2020-04-17 17:38:57 +0200

    Add files via upload

---
+++ II1302Projektgrupp3-master/aurora.ex
@@ -0,0 +1,8 @@
+defmodule Aurora
+  def chalmers do
+    IO.puts("Aurora Borealis?! At this time of year, at this time of DAY, in this part of the country, localized ENTIRELY within your kitchen?")
+  end
+  def skinner do
+    IO.puts("Yes!")
+  end
+end
