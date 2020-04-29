d99d18dcac44a95506b186fbd06957b125437308
albroms <albroms@kth.se>
2020-04-29 11:05:50 +0200

    unique page components and test script done.

---
+++ ProjektConnection/pages/alert_input.js
@@ -0,0 +1,28 @@
+//PLEASE NOTE THAT THESE HASHING FUNCTIONS ARE NOT SECURE
+//THEY SHOULD NOT BE USED FOR ANY SERIOUS CRYPTOGRAPHY UNLESS
+//YOU WANT SENSITIVE DATA TO BE ACCESSED BY UNAUTHORIZED USERS
+
+function hash(string_input){
+  let hash = 5059;
+  if(string_input.length == 0) return hash;
+  for(i=0; i<string_input.length; i++){
+    char = string_input.charCodeAt(i);
+    hash = ((hash << 5) - hash) + char;
+    hash = hash & hash;
+  }
+  return hash;
+}
+function hash_and_salt(text, salt){
+  debugger;
+  input_to_salt = text + salt;
+  salted_hash = hash(input_to_salt);
+  return salted_hash;
+}
+
+//show input
+function show_input(){
+  let user = document.forms["login_form"]["user"].value;
+  let pass = document.forms["login_form"]["password"].value;
+  let secret = hash_and_salt(pass, user);
+  alert("username: " + user + ", secret(should not match your password input): " + secret);
+}
